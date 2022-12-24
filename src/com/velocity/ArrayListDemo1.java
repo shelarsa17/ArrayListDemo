755d41c3fedc0061f062e96bf4ce618f4a9466cc
shelarsa17 <shelarsa17@gmail.com>
2022-12-24 17:48:08 +0530

    first commit : added arraylistdemo code

---
+++ src/com/velocity/ArrayListDemo1.java
@@ -0,0 +1,20 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo1 {
+	
+	public static void main(String[] args) {
+		
+		List<String> list=new ArrayList<String>();
+		
+		list.add("ram");
+		list.add("santosh");
+		list.add("sachin");
+		
+		System.out.println(list);
+		
+	}
+
+}
