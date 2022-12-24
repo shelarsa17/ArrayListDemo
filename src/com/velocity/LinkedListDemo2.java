abb2ad5264b08c4912ba10d0ae2c0aef7578147d
shelarsa17 <shelarsa17@gmail.com>
2022-12-24 21:04:00 +0530

    second commit : added code for linkedlist

---
+++ src/com/velocity/LinkedListDemo2.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo2 {
+	
+	public static void main(String[] args) {
+		
+		
+		LinkedList<Integer> linkedList=new LinkedList<Integer>();
+		linkedList.add(10);
+		linkedList.add(20);
+		linkedList.add(30);
+		linkedList.add(40);
+		
+		System.out.println(linkedList);
+	}
+
+}
